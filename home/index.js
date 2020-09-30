89ee05cecdb1f60dd19271e910a0823ed6d6789f
Carlos Rocha <carlinhozrocha@gmail.com>
2020-09-30 00:19:03 -0300

    primeira etapa

---
+++ home/index.js
@@ -0,0 +1,29 @@
+const elementos = document.querySelectorAll("[data-anima]");
+const animacaoClass="animacao";
+var primeiro = document.getElementById("container-primario");
+var segundo = document.getElementById("container-secundario");
+var terceiro = document.getElementById("container-terciario");
+
+function start(){
+	primeiro.style.opacity = "1";
+	console.log(position);
+}
+
+function animaScroll(){
+	const position = window.pageYOffset;
+	if(position <= 50){
+		primeiro.style.opacity = "1";
+		segundo.style.opacity = "0";
+		terceiro.style.opacity = "0";
+	};
+	if(position >= 300){
+		primeiro.style.opacity = "0";
+		segundo.style.opacity = "1";
+		terceiro.style.opacity = "0";
+	};
+	if(position >= 800){
+		primeiro.style.opacity = "0";
+		segundo.style.opacity = "0";
+		terceiro.style.opacity = "1";
+	};
+}
